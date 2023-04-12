f87fb00ae0a093f83aa0a282feaf09bc30ec7ce2
Excell-09 <excell.agcb12@gmail.com>
2023-04-12 17:51:39 +0700

    create main

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,12 @@ import User from "./User";
 
 const Header = () => {
   return (
-    <header className='flex justify-between items-center py-3 px-5 text-sm text-gray-700'>
-      <div className="flex space-x-4 items-center">
+    <header className='flex justify-between items-center py-3 px-5 text-sm text-gray-700 fixed right-0 w-full'>
+      <div className="flex space-x-3 items-center">
         <p className="link">About</p>
         <p className="link">Store</p>
       </div>
-      <div className="flex space-x-4 items-center">
+      <div className="flex space-x-3 items-center">
         <p className="link">Gmail</p>
         <p className="link">Images</p>
         <User/>
--- components/User.js
+++ components/User.js
@@ -9,14 +9,18 @@ const User = () => {
     return (
       <img
         onClick={() => signOut()}
-        className='cursor-pointer rounded-full h-14 w-14 p-2 hover:bg-gray-300'
+        className='cursor-pointer rounded-full h-14 w-14 p-2 hover:bg-gray-300 transition-all duration-300'
         src={session?.user?.image}
         alt='profile'
         referrerPolicy='no-referrer'
       />
     );
   }
-  return <button className='link' onClick={() => router.push('/auth/signin')}>Sign In</button>;
+  return (
+    <button className='text-white text-base font-semibold bg-blue-500 px-3 py-1 rounded-md hover:ring transition-all duration-150' onClick={() => router.push('/auth/signin')}>
+      Sign In
+    </button>
+  );
 };
 
 export default User;
