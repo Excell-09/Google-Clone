5d84c9adf1775b7c2495909fd7922806ba22ba8c
Excell-09 <excell.agcb12@gmail.com>
2023-04-20 18:28:13 +0700

    migrate to typescript

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,13 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret:process.env.SECRET,
+  secret: process.env.SECRET,
   pages: {
     signIn: '/auth/signin',
   },
